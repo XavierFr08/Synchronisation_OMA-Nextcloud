8bdaa0bd4275fc86e5b5a5f13bffd2bc15e40584
XavierFr08 <xavier@fifetxav.net>
2026-02-20 14:54:24 +0100

    Create mount-piusb-ro.sh

---
+++ usr/local/bin/mount-piusb-ro.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# filepath: /usr/local/bin/mount-piusb-ro.sh
+
+# ------------------------------------------------------------
+# Script de montage local d'une image USB simulée (piusb.img)
+# sur le Raspberry Pi, en lecture seule, pour permettre la
+# lecture des fichiers copiés par une machine industrielle.
+# ------------------------------------------------------------
+
+set -e
+
+IMG="/piusb.img"                # Chemin de l'image disque à monter
+MOUNTPOINT="/mnt/piusb"         # Point de montage local sur le Pi
+
+mkdir -p "$MOUNTPOINT"
+
+# Cherche le device loopback déjà associé à l'image (si existant)
+DEVICE=$(losetup -j "$IMG" | awk -F: '{print $1}' | head -n1)
+
+if [ -z "$DEVICE" ]; then
+    # Si aucun device n'est associé, crée un nouveau device loopback
+    DEVICE=$(losetup -f --show "$IMG")
+fi
+
+# Monte l'image en lecture seule, avec les bons UID/GID pour l'utilisateur "xavier"
+mount -o ro,uid=$(id -u xavier),gid=$(id -g xavier) "$DEVICE" "$MOUNTPOINT"
+
+echo "Image $IMG montée en lecture seule sur $MOUNTPOINT via $DEVICE"
